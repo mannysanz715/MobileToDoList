ae73dd19951baa935b6328b7b5c6b470d4d77f92
MannySanz <mannysanz715@gmail.com>
2023-06-26 14:16:08 -0700

    Create Mobile To Do App and mess around to learn react native

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { ScrollView, StyleSheet, Button, Text, View } from 'react-native';
 
 export default function App() {
+const [count, setCounter] = useState(0)
+function onButtonPress(){
+  return setCounter(count + 1)
+}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView >
+      <View style={styles.container}>
+        <Text>Daily To Do's</Text>
+        <Text>The app that Lets you keep track of daily items. All in one organized place!</Text>
+          <Text >Hello</Text>
+        <View style={styles.buttonStyle}>
+          <Button onPress={onButtonPress} title={`${count}`}/>
+        </View>  
+      </View>
+      <View>
+
+      </View>
+    </ScrollView>
   );
 }
 
@@ -14,7 +27,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent : 'center',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 110,
   },
+  buttonStyle: {
+    backgroundColor: '#63C5DA',
+    width: 100,
+    height: 50,
+    textAlign : 'center',
+    justifyContent : 'center',
+    alignItems: "center"
+
+  }
 });
